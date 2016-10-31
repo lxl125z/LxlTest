45a7b9035e1e6928cb2f422c66e15cbeae65a92f
Liu XiaoLiang <Jingpeng@lxlmacbook-air.local>
2016-10-31 16:22:40 +0800

    增加pod文件

---
+++ LxlTest/PodTest/LxlTestMacro.h
@@ -0,0 +1,14 @@
+//
+//  LxlTestMacro.h
+//  LxlTest
+//
+//  Created by 刘晓亮 on 16/10/31.
+//  Copyright © 2016年 集美惠. All rights reserved.
+//
+
+#ifndef LxlTestMacro_h
+#define LxlTestMacro_h
+
+//姬帅成功了
+
+#endif /* LxlTestMacro_h */
